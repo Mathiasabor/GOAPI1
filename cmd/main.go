76aaedb45625ce0647b12c59a8b89911b54239f7
Mathiasabor <103271868+Mathiasabor@users.noreply.github.com>
2023-08-21 06:12:40 +0000

    Vendor

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt" 
-	//"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 
 )
 
 func main(){
 	fmt.Println("le nouveau api commence")
+	router:=gin.Default()
+	router.GET("/", func(c *gin.Context){
+		c.JSON(200,gin.H{
+			"message":"hello world",
+		})
+	})
+	router.Run(":5000")
 }
